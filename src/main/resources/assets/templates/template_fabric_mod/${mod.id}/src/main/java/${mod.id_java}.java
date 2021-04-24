bd761063de2cab8ffae02e91e09d0ed4e533aa1e
Daniel Orr <orrdaniel73@gmail.com>
2021-04-24 11:13:32 +0100

    fabric mod template

---
+++ src/main/resources/assets/templates/template_fabric_mod/${mod.id}/src/main/java/${mod.id_java}.java
@@ -0,0 +1,29 @@
+package ${mod.maven_group};
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ${mod.id_java} implements ModInitializer {
+    public static final String MOD_ID = "${mod.id}";
+    public static final String MOD_NAME = "${mod.name}";
+
+    public static Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+	  @Override
+    public void onInitialize() {
+        log("Initializing");
+
+        //
+
+        log("Initialized");
+    }
+
+    public static void log(Level level, String message){
+        LOGGER.log(level, "[" + MOD_NAME + "] " + message);
+    }
+    public static void log(String message) {
+        log(Level.INFO, message);
+    }
+}
